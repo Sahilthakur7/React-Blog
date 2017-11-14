f4e1b85fffaa9e15991ad1017d59f43472d6cd44
SahilThakur7 <sahilrd07@gmail.com>
2017-11-14 10:24:20 +0530

    fields added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,13 +2,57 @@ import React from 'react';
 import { Field, reduxForm} from 'redux-form';
 
 class PostsNew extends React.Component{
+    renderField(field){
+        return(
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input type="text" className="form-control"
+                    {...field.input} />
+                {field.meta.error}
+            </div>
+        );
+    }
     render(){
         return(
             <div>
-                PostsNew!
+                <form>
+                    <Field
+                        label="Title"
+                        name="title"
+                        component={this.renderField} 
+                    />
+                    <Field
+                        label="Categories"
+                        name="categories"
+                        component={this.renderField} 
+                    />
+                    <Field
+                        label="Content"
+                        name="content"
+                        component={this.renderField} 
+                    />
+                </form>
             </div>
         );
     }
 }
 
-export default PostsNew;
+function validate(values){
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+    if (!values.categories) {
+        errors.categories = "Enter some categories!";
+    }
+    if (!values.content) {
+        errors.content = "Enter some content!";
+    }
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(PostsNew);
+
